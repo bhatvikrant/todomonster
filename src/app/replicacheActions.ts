95993bda473bfa6e987c9f549ff9d79766a640b2
Vikrant Bhat <vik.bhat223@gmail.com>
2024-01-23 23:57:57 +0530

    Add replicacheActions.ts file for listening to pusher events

---
+++ src/app/replicacheActions.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import Pusher from "pusher-js";
+import type { Replicache } from "replicache";
+import type { Mutators } from "@replicache/mutators";
+
+export const listen = (rep: Replicache<Mutators> | null) => {
+  if (rep) {
+    console.log("👂 listening");
+    Pusher.logToConsole = true;
+    const pusher = new Pusher("app-key", {
+      cluster: "",
+      wsHost: "127.0.0.1",
+      wsPort: 6001,
+      forceTLS: false,
+      enabledTransports: ["ws", "wss"],
+    });
+    const channel = pusher.subscribe("default-channel");
+    channel.bind("poke-event", async () => {
+      console.log("🫰 got poked");
+      try {
+        await rep.pull();
+      } catch (error) {
+        console.log("rep.pull() failed");
+      }
+    });
+  }
+};
