e42fe4e45b8daf68e638d8279214a8ded37f8083
Vikrant Bhat <vik.bhat223@gmail.com>
2024-01-23 23:29:42 +0530

    Add database initialization script to package.json

---
+++ src/server/db/initializeDatabase.ts
@@ -0,0 +1,23 @@
+import db from "./index";
+import { replicacheMeta } from "./schema";
+
+// consider default values https://orm.drizzle.team/docs/indexes-constraints#default
+
+const initializeDatabase = () => {
+  const rows = db.select().from(replicacheMeta).prepare().all();
+
+  if (rows.length === 0) {
+    // Insert a row to set the global database version in the replicacheServer table.
+    console.log("initializing database...");
+    db.insert(replicacheMeta)
+      .values({
+        key: "schemaVersion",
+        value: "1",
+      })
+      .run();
+  } else {
+    console.log("database already initialized.");
+  }
+};
+
+initializeDatabase();
