0bf6e98c0a1a11fc86e90424c63feabea54b1216
Vikrant Bhat <vik.bhat223@gmail.com>
2024-01-26 01:16:42 +0530

    Fix eslint errors and create labels for GitHub issues

--- src/app/api/qstash/route.ts
+++ src/app/api/qstash/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type NextRequest, NextResponse } from "next/server";
 
@@ -17,7 +19,6 @@ type BodyType = {
 
 async function handler(request: NextRequest) {
   const body: BodyType = await request.json();
-  console.log("body:", body);
 
   const resp = await handleGithubIssuManipulation(body);
 
@@ -39,31 +40,48 @@ async function handleGithubIssuManipulation({ type, data, userID }: BodyType) {
         return { message: `Todo id is missing`, status: 500, success: false };
       }
       try {
+        try {
+          await Promise.all(
+            [
+              {
+                id: new Date().getTime(),
+                name: `userID_${userID}`,
+                color: "ff0000",
+                description: "Todo created by user's Id",
+              },
+              {
+                id: new Date().getTime() + 1,
+                name: `listID_${data.listID}`,
+                color: "0000ff",
+                description: "Todo is part of this list",
+              },
+            ].map(async (labelData) =>
+              octokit.rest.issues.createLabel({
+                owner,
+                repo,
+                ...labelData,
+                mediaType: {
+                  previews: ["symmetra"],
+                },
+              }),
+            ),
+          );
+        } catch (error) {
+          console.log("error:", error);
+        }
         const res = await octokit.rest.issues.create({
           owner,
           repo,
           title: data.text!,
-          labels: [
-            {
-              name: `userID=${userID}`,
-              color: "ff0000",
-              description: "Todo created by user's Id",
-            },
-            {
-              name: `listID=${data.listID}`,
-              color: "00ff00",
-              description: "Todo is part of this list",
-            },
-          ],
+          labels: [`userID_${userID}`, `listID_${data.listID}`],
         });
-        console.log("res:", res);
-        const dbRes = await db.insert(todoIssueMapping).values({
+
+        await db.insert(todoIssueMapping).values({
           id: nanoid(),
           todoID: data.id,
           issueNodeID: res.data.node_id,
           issueNumber: res.data.number,
         });
-        console.log("dbRes:", dbRes);
 
         return { data: res, status: res.status, success: true };
       } catch (error) {
